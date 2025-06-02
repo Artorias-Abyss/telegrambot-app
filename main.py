936ee1de53cf72888004f1d76d60b70d95d4feca
Artorias <konon.mama4356@gmail.com>
2025-06-02 23:13:45 +0300

    finish 3 lesson

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import telebot
+import webbrowser
 
-bot = telebot.TeleBot(config.TOKEN)
+bot = telebot.TeleBot('7843121335:AAFsLS-YUMnxg9clLWZl4oxe-jYSGNHvmyo')
+
+@bot.message_handler(commands=['site', 'website'])
+def main (message):
+    webbrowser.open('https://ru.pinterest.com')
+
+@bot.message_handler(commands=['start', 'hello'])
+def main (massage):
+    bot.send_message(massage.chat.id, 'I am Malenia. Blade of Miquella. And I have never known defeat.')
+
+@bot.message_handler(commands=['help'])
+def main (massage):
+    bot.send_message(massage.chat.id, 'Helping menu.')
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'привет' :
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+bot.polling(non_stop=True)
